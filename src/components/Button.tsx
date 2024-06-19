081eb458648f50f89ec67eec00aafd9370975408
Антон Марченко <anton.mrch1998@gmail.com>
2024-06-19 10:05:02 +0300

    feat: button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export const Button = () => {
-  <View>
+  <View style={{ backgroundColor: 'red' }}>
     <Text>Button</Text>
   </View>;
 };
